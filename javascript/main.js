b2eedf04b45d866637cc367a2c80bbadef8fb710
mugri-ndeh <frunwindeh@yahoo.com>
2022-08-29 16:15:28 +0100

    add and sub buttins works js

--- javascript/main.js
+++ javascript/main.js
@@ -7,5 +7,42 @@ function handleTextOverflow(length, textClass) {
   });
 }
 
-handleTextOverflow(13, '.food-card-text1');
-handleTextOverflow(15, '.food-card-text');
+handleTextOverflow(13, ".food-card-text1");
+handleTextOverflow(15, ".food-card-text");
+
+// Cart page
+var addCartQty = document.querySelectorAll(".add-cart-qty");
+var removeCartQty = document.querySelectorAll(".subtract-cart-qty");
+var cartQty = document.querySelectorAll(".cart-item-qty");
+
+if (addCartQty) {
+  addCartQty.forEach(function (element, index) {
+    element.addEventListener("click", function () {
+      cartQty[index].value++;
+    });
+  });
+
+  removeCartQty.forEach(function (element, index) {
+    element.addEventListener("click", function () {
+      if (cartQty[index].value == 1) {
+      } else {
+        cartQty[index].value--;
+      }
+    });
+  });
+}
+
+//add to cart modal
+var addFoodQty = document.getElementById("add-food-qty");
+var removeFoodQty = document.getElementById("subtract-food-qty");
+var foodQty = document.getElementById("food-qty");
+
+if (addFoodQty) {
+  addFoodQty.addEventListener("click", () => foodQty.value++);
+  removeFoodQty.addEventListener("click", function () {
+    if (foodQty.value == 1) {
+    } else {
+      foodQty.value--;
+    }
+  });
+}
