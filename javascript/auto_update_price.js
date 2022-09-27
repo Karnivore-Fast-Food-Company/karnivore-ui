afeea536814147ae894e0e809d2ae3b7b12c8a67
mugri-ndeh <frunwindeh@yahoo.com>
2022-09-27 20:25:57 +0100

    fixed calculations

--- javascript/auto_update_price.js
+++ javascript/auto_update_price.js
@@ -6,88 +6,49 @@ const addFoodQty = document.getElementById('add-food-qty');
 const removeFoodQty = document.getElementById('subtract-food-qty');
 const foodQty = document.getElementById('food-qty');
 const total = document.getElementById('total-price');
-const sizes = document.querySelectorAll('.size');
+const radios = document.querySelectorAll('.size');
 let complementSelected = '';
-let size = 'SMALL';
-
-const SIZES = {
-    SMALL: 500,
-    MEDIUM: 700,
-    LARGE: 1000,
-};
-
-const COMPLEMENTS = {
-    'French fries': 300,
-    'Plantain Tape': 500,
-};
+let size = 0;
 
 // update price function
-const updatePrice = (size, quantity, complement) => {
-    totalPrice = (size + complement) * quantity;
+const updatePrice = (size, quantity) => {
+    console.log('SIZE: ' + size)
+    totalPrice = size * quantity;
     total.innerHTML = totalPrice.toString();
 };
 
 // initialisation
 function init() {
-    const radios = document.getElementsByTagName('input');
-    let value;
+    const radios = document.querySelectorAll('.size');
     for (let i = 0; i < radios.length; i += 1) {
-        if (radios[i].type === 'radio' && radios[i].checked) {
-            // get value, set checked flag or do whatever you need to
-            value = `label[for=${radios[i].id}]`;
-            const label = document.querySelector(value);
-            const text = label.innerHTML.trim();
-            complementSelected = text;
-            updatePrice(SIZES[size], 1, COMPLEMENTS[complementSelected]);
+        if (radios[i].checked) {
+            size = parseInt(radios[i].value, 10);
+            updatePrice(size, 1);
         }
     }
 }
 
-// update price when new size is selected
-sizes.forEach((element) => element.addEventListener('click', () => {
-    size = element.innerText.trim().toUpperCase();
-    updatePrice(SIZES[size], parseInt(foodQty.innerText, 10), COMPLEMENTS[complementSelected]);
-}));
-
 init();
 
-// hanlde radio button click
-const handleClick = () => {
-    // radios
-    const radios = document.getElementsByTagName('input');
-    let value;
-    for (let i = 0; i < radios.length; i += 1) {
-        if (radios[i].type === 'radio' && radios[i].checked) {
-            value = `label[for=${radios[i].id}]`;
-            const label = document.querySelector(value);
-            const text = label.innerHTML.trim();
-            complementSelected = text;
-            updatePrice(SIZES[size], parseInt(foodQty.innerText, 10), COMPLEMENTS[complementSelected]);
-        }
-    }
-};
-
-// adding even listerners to rasio buttons
-const radios = document.getElementsByTagName('input');
+// adding even listerners to radio buttons
 for (let i = 0; i < radios.length; i += 1) {
-    if (radios[i].type === 'radio') {
-        radios[i].onclick(() => {
-            handleClick();
-        });
-    }
+    radios[i].addEventListener('click', () => {
+        size = parseInt(radios[i].value);
+        updatePrice(size, parseInt(foodQty.innerText, 10));
+    });
 }
 
 // adding or subtracting food quantity
 if (addFoodQty) {
     addFoodQty.addEventListener('click', () => {
         foodQty.innerText = (parseInt(foodQty.innerText, 10) + 1).toString();
-        updatePrice(SIZES[size], parseInt(foodQty.innerText, 10), COMPLEMENTS[complementSelected]);
+        updatePrice(size, parseInt(foodQty.innerText, 10));
     });
     removeFoodQty.addEventListener('click', () => {
         if (foodQty.innerText !== '1') {
             foodQty.innerText = (parseInt(foodQty.innerText, 10) - 1).toString();
         }
-        updatePrice(SIZES[size], parseInt(foodQty.innerText, 10), COMPLEMENTS[complementSelected]);
+        updatePrice(size, parseInt(foodQty.innerText, 10));
     });
-    updatePrice(SIZES[size], parseInt(foodQty.innerText, 10), COMPLEMENTS[complementSelected]);
+    updatePrice(size, parseInt(foodQty.innerText, 10));
 }
